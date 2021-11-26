66c81924ea3afef3d7a82869e0cd4baf82386c32
Daria Aleksanova <darishubko@gmail.com>
2021-11-26 12:06:38 +0100

    fix: make show more button work

--- culinary-website-app/src/pages/MainPage/MainPage.tsx
+++ culinary-website-app/src/pages/MainPage/MainPage.tsx
@@ -1,26 +1,47 @@
 import RecipePreviewComponent from 'components/RecipePreviewComponent/RecipePreviewComponent';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router';
 import { loadRecipesPreviews } from 'store/recipesPreviews/actions';
 import { RootState } from 'store/store';
 import styles from './MainPage.module.css';
 
 const MainPage = () => {
   const recipePreviewItems = useSelector((state: RootState) => state.recipesPreviews.recipesPreviews);
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const query = new URLSearchParams(location.search);
+  const totalRecipes = query.get('totalRecipes') || '5';
+  // почему вызывается два раза?
+  // console.log(totalRecipes)
+
+  const [visibleRecipesAmount, setVisibleRecipesAmount] = useState(Number(totalRecipes));
 
   useEffect(() => {
     dispatch(loadRecipesPreviews());
   }, [dispatch]);
 
+  const handleShowMoreButtonClick = () => {
+    const query = new URLSearchParams(location.search);
+    const totalRecipes = query.get('totalRecipes') || '5';
+    const newTotalRecipes = parseInt(totalRecipes, 10) + 5;
+    query.set('totalRecipes', newTotalRecipes.toString());
+
+    navigate(`${location.pathname}?${query.toString()}`);
+
+    setVisibleRecipesAmount(prevState => prevState + 5);
+  };
+
   return (
     <div className={styles.mainPage}>
       <h1 className={styles.mainPageTitle}>Recipes</h1>
       <ul className={styles.recipesList}>
-        {recipePreviewItems.map(item => <RecipePreviewComponent key={item.id} recipePreview={item} />)}
+        {recipePreviewItems.slice(0, visibleRecipesAmount).map(item => <RecipePreviewComponent key={item.id} recipePreview={item} />)}
       </ul>
       <div className={styles.mainPageActions}>
-        <button className={styles.showMoreButton}>Show more</button>
+        <button className={styles.showMoreButton} onClick={handleShowMoreButtonClick}>Show more</button>
       </div>
     </div>
   );
