511ab956a8700ca655a7c4794a9f15d866b07db2
Daria Aleksanova <darishubko@gmail.com>
2021-12-01 15:48:37 +0100

    feature: make search by recipe title work

--- culinary-website-app/src/components/SearchBarComponent/SearchBarComponent.tsx
+++ culinary-website-app/src/components/SearchBarComponent/SearchBarComponent.tsx
@@ -2,15 +2,34 @@ import styles from './SearchBarComponent.module.css';
 import SearchFilterComponent from 'components/SearchFilterComponent';
 import { useState } from 'react';
 
-const SearchBarComponent = () => {
+type Props = {
+  searchBarValue: string;
+  onSearchBarValueChange: (newValue: string) => void;
+  onSearchSubmitButtonClick: (searchBarValue: string) => void;
+};
+
+const SearchBarComponent = ({ searchBarValue, onSearchBarValueChange, onSearchSubmitButtonClick }: Props) => {
   const [isSearchFilterOpen, setIsSearchFilterOpen] = useState(false);
 
   return (
     <div className={styles.searchBarComponent}>
     <div className={styles.searchBarContainer}>
-      <input className={styles.searchInput} type='text' placeholder='What do you want to cook today?'></input>
-      <button className={styles.searchFilterButton} disabled={isSearchFilterOpen} onClick={() => setIsSearchFilterOpen(true)}>Filter</button>
-      <button className={styles.searchSubmitButton}>Search</button>
+      <input 
+        className={styles.searchInput} 
+        type='text' 
+        value={searchBarValue}
+        placeholder='What do you want to cook today?' 
+        onChange={(event) => onSearchBarValueChange(event.target.value)}
+      >
+      </input>
+      <button 
+        className={styles.searchFilterButton} 
+        disabled={isSearchFilterOpen} 
+        onClick={() => setIsSearchFilterOpen(true)}
+      >
+        Filter
+      </button>
+      <button className={styles.searchSubmitButton} onClick={() => onSearchSubmitButtonClick(searchBarValue)}>Search</button>
     </div>
     {isSearchFilterOpen && (
       <>
--- culinary-website-app/src/pages/MainPage/MainPage.tsx
+++ culinary-website-app/src/pages/MainPage/MainPage.tsx
@@ -6,10 +6,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router';
 import { loadRecipesPreviews } from 'store/recipesPreviews/actions';
 import { RootState } from 'store/store';
+import { RecipePreview } from 'types/recipePreview';
 import styles from './MainPage.module.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 const MainPage = () => {
-  const recipePreviewItems = useSelector((state: RootState) => state.recipesPreviews.recipesPreviews);
+  const recipesPreviewsList = useSelector((state: RootState) => state.recipesPreviews.recipesPreviews);
   const favouriteRecipesList = useSelector((state: RootState) => state.favouriteRecipes.favouriteRecipes);
   const areRecipePreviewsLoaded = useSelector((state: RootState) => state.recipesPreviews.isLoaded);
 
@@ -21,10 +25,21 @@ const MainPage = () => {
   const totalRecipes = query.get('totalRecipes') || '5';
 
   const [visibleRecipesAmount, setVisibleRecipesAmount] = useState(Number(totalRecipes));
+  const [searchBarValue, setSearchBarValue] = useState('');
+  const [searchResults, setSearchResults] = useState<RecipePreview[] | null>(null);
 
   useEffect(() => {
     dispatch(loadRecipesPreviews());
   }, [dispatch]);
+
+  const handleSearchBarValueChange = (newValue: string) => {
+    setSearchBarValue(newValue);
+  };
+
+  const searchRecipes = (searchBarValue: string) => {
+    const filteredRecipes = recipesPreviewsList.filter(recipe => recipe.title.toLowerCase().includes(searchBarValue.toLowerCase()));
+    setSearchResults(filteredRecipes);
+  };
 
   const handleShowMoreButtonClick = () => {
     const query = new URLSearchParams(location.search);
@@ -42,17 +57,36 @@ const MainPage = () => {
       {!areRecipePreviewsLoaded ? 
         <Loader /> : (
           <div className={styles.mainPageContainer}>
-            <SearchBarComponent />
+            <SearchBarComponent searchBarValue={searchBarValue} onSearchBarValueChange={handleSearchBarValueChange} onSearchSubmitButtonClick={searchRecipes}/>
             <h1 className={styles.mainPageTitle}>Recipes</h1>
             <ul className={styles.recipesList}>
-              {recipePreviewItems.slice(0, visibleRecipesAmount).map(recipe =>
-                favouriteRecipesList.find(favouriteRecipe => favouriteRecipe.id === recipe.id) ?
-                <RecipePreviewComponent key={recipe.id} recipePreview={recipe} isFavourite={true}/> :
-                <RecipePreviewComponent key={recipe.id} recipePreview={recipe} isFavourite={false}/>
+              {((searchResults === null) || searchBarValue === '') && (
+                recipesPreviewsList.slice(0, visibleRecipesAmount).map(recipe =>
+                  favouriteRecipesList.find(favouriteRecipe => favouriteRecipe.id === recipe.id) ?
+                  <RecipePreviewComponent key={recipe.id} recipePreview={recipe} isFavourite={true}/> :
+                  <RecipePreviewComponent key={recipe.id} recipePreview={recipe} isFavourite={false}/>
+                )
               )}
+              {(searchResults !== null && searchResults.length !== 0) && (
+                searchResults.slice(0, visibleRecipesAmount).map(recipe =>
+                  favouriteRecipesList.find(favouriteRecipe => favouriteRecipe.id === recipe.id) ?
+                  <RecipePreviewComponent key={recipe.id} recipePreview={recipe} isFavourite={true}/> :
+                  <RecipePreviewComponent key={recipe.id} recipePreview={recipe} isFavourite={false}/>
+                )
+              )}
+              {(searchResults !== null && searchResults.length === 0 && searchBarValue !== '') && 
+                <h2 className={styles.noResultsTitle}>No results found!</h2>
+              }
             </ul>
             <div className={styles.mainPageActions}>
-              <button className={styles.showMoreButton} onClick={handleShowMoreButtonClick}>Show more</button>
+              <button className={cx({
+                showMoreButton: true,
+                showMoreButtonHidden: searchResults !== null && searchResults.length === 0 && searchBarValue !== '',
+                })} 
+                onClick={handleShowMoreButtonClick}
+              >
+                Show more
+              </button>
             </div>
           </div>
         )
