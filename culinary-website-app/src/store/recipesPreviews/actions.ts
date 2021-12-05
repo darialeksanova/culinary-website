d5aaaa0522f3d2fcd69aa50eda17d70aa26dd11a
Daria Aleksanova <darishubko@gmail.com>
2021-12-05 19:17:37 +0100

    fix: make page url address single source of truth

--- culinary-website-app/src/store/recipesPreviews/actions.ts
+++ culinary-website-app/src/store/recipesPreviews/actions.ts
@@ -1,6 +1,9 @@
 import { API_URL, API_KEY } from "constants/index";
 import { Dispatch } from "redux";
+import { PaginatedSearchResults } from "types/paginatedSearchResults";
 import { RecipePreview } from "types/recipePreview";
+import { SearchFilterValue } from "types/searchFilterValue";
+import { SearchParams } from "types/searchParams";
 import { LoadRecipesPreviewsFailureAction, LoadRecipesPreviewsStartedAction, LoadRecipesPreviewsSuccessAction, RecipesPreviewsAction } from "./types";
 
 const startRecipesPreviewLoading = (): LoadRecipesPreviewsStartedAction => {
@@ -23,18 +26,77 @@ const setError = (error: Error): LoadRecipesPreviewsFailureAction => {
   };
 };
 
-export const loadRecipesPreviews = () => (dispatch: Dispatch) => {
+const getDietFilterValues = (searchFilterValues: SearchFilterValue): string[] => {
+  const dietFilterValues = [];
+
+  if (searchFilterValues.isVegetarian) {
+    dietFilterValues.push('Vegetarian');
+  }
+
+  if (searchFilterValues.isVegan) {
+    dietFilterValues.push('Vegan');
+  }
+
+  if (searchFilterValues.isGlutenFree) {
+    dietFilterValues.push('Gluten Free');
+  }
+
+  return dietFilterValues;
+};
+
+const composeURLSearchParams = (searchParams: SearchParams): string => {
+  const dietFilterValues = getDietFilterValues(searchParams.filters);
+  const complexSearchURLParams: { [key: string]: string } = {
+    apiKey: API_KEY,
+  };
+
+  if (dietFilterValues.length !== 0) {
+    complexSearchURLParams.diet = dietFilterValues.join(',');
+  }
+
+  if (searchParams.filters.isDairyFree) {
+    complexSearchURLParams.intolerances = 'Dairy';
+  }
+
+  if (searchParams.searchInput !== '') {
+    complexSearchURLParams.titleMatch = searchParams.searchInput.toLowerCase();
+  }
+
+  return Object.entries(complexSearchURLParams).map(([paramKey, paramValue]) => `${paramKey}=${paramValue}`).join('&');
+};
+
+export const loadRecipesPreviews = (searchParams: SearchParams) => (dispatch: Dispatch) => {
   dispatch(startRecipesPreviewLoading());
-  fetch(`${API_URL}/recipes/complexSearch?apiKey=${API_KEY}&number=2000`)
-  .then(response => {
-    if(response.ok) {
-      return response.json();
+  Promise.all([
+    fetch(`${API_URL}/recipes/findByIngredients?apiKey=${API_KEY}&ingredients=${searchParams.searchInput}&number=100`)
+      .then(response => {
+        if(response.ok) {
+          return response.json() as Promise<RecipePreview[]>;
+        }
+
+        throw new Error('Error on dish ingredients fetch!');
+      }),
+    fetch(`${API_URL}/recipes/complexSearch?${composeURLSearchParams(searchParams)}&number=100`)
+      .then(response => {
+        if(response.ok) {
+          return response.json() as Promise<PaginatedSearchResults<RecipePreview>>;
+        }
+
+        throw new Error('Error on recipe preview items fetch!');
+      }),
+  ])
+  .then(([ foundByIngredients, foundByOtherConditions ]) => {
+    let searchResult = [...foundByOtherConditions.results];
+
+    if (searchParams.searchInput !== '') {
+      searchResult.filter(
+        recipeByOtherConditions => foundByIngredients.find(
+          recipeByIngredients => recipeByIngredients.id === recipeByOtherConditions.id,
+        ),
+      );
     }
 
-    throw new Error('Error on recipe preview items fetch!');
-  })
-  .then((recipePreviewItemsObj: {results: RecipePreview[]}) => {
-    dispatch(setRecipesPreviews(recipePreviewItemsObj['results']))
+    dispatch(setRecipesPreviews(searchResult.slice(0, searchParams.totalRecipes)));
   })
   .catch((error: Error) => {
     console.log('Source is not reachable!');
--- culinary-website-app/src/store/store.ts
+++ culinary-website-app/src/store/store.ts
@@ -3,12 +3,14 @@ import { recipesPreviewsReducer } from './recipesPreviews/recipesPreviewsReducer
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { favouriteRecipesReducer } from './favouriteRecipes/favouriteRecipesReducer';
+import { searchFilterValuesReducer } from './searchFilterValues/searchFilterValuesReducer';
 import { themeReducer } from './theme/themeReducer';
 
 const rootReducer = combineReducers({
   recipesPreviews: recipesPreviewsReducer,
   favouriteRecipes: favouriteRecipesReducer,
   theme: themeReducer,
+  filterValues: searchFilterValuesReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
