51503978596b22295d43c525cb9e4b87e9db9668
Daria Aleksanova <darishubko@gmail.com>
2021-12-05 19:19:43 +0100

    feature: add reset button and modify filter button

--- culinary-website-app/src/components/SearchBarComponent/SearchBarComponent.tsx
+++ culinary-website-app/src/components/SearchBarComponent/SearchBarComponent.tsx
@@ -2,19 +2,18 @@ import styles from './SearchBarComponent.module.css';
 import SearchFilterComponent from 'components/SearchFilterComponent';
 import { useCallback, useState } from 'react';
 import classNames from 'classnames/bind';
-import { SearchFilterValues } from 'types/searchFilter';
+import filterIcon from 'assets/filter.svg';
 
 const cx = classNames.bind(styles);
 
 type Props = {
   searchBarValue: string;
-  searchFilterValues: SearchFilterValues;
-  setSearchFilterValues: (filterValues: SearchFilterValues) => void;
   onSearchBarValueChange: (newValue: string) => void;
   onSearchSubmitButtonClick: (searchBarValue: string) => void;
+  onResetButtonClick: () => void;
 };
 
-const SearchBarComponent = ({ searchBarValue, searchFilterValues, setSearchFilterValues, onSearchBarValueChange, onSearchSubmitButtonClick }: Props) => {
+const SearchBarComponent = ({ searchBarValue, onSearchBarValueChange, onSearchSubmitButtonClick, onResetButtonClick }: Props) => {
   const [isSearchFilterOpen, setIsSearchFilterOpen] = useState(false);
 
   const handleFilterButtonClick = useCallback(() => isSearchFilterOpen? setIsSearchFilterOpen(false) : setIsSearchFilterOpen(true), [isSearchFilterOpen]);
@@ -22,28 +21,36 @@ const SearchBarComponent = ({ searchBarValue, searchFilterValues, setSearchFilte
   return (
     <div className={styles.searchBarComponent}>
     <div className={styles.searchBarContainer}>
-      <input 
-        className={styles.searchInput} 
-        type='text' 
-        value={searchBarValue}
-        placeholder='What do you want to cook today?' 
-        onChange={(event) => onSearchBarValueChange(event.target.value)}
-      >
-      </input>
+      <div className={styles.searchInputContainer}>
+        <input 
+          className={styles.searchInput} 
+          type='text' 
+          value={searchBarValue}
+          placeholder='What do you want to cook today?' 
+          onChange={(event) => onSearchBarValueChange(event.target.value)}
+        >
+        </input>
+        <button 
+          className={cx({
+            searchFilterButton: true,
+            searchFilterButtonClicked: isSearchFilterOpen,
+          })}
+          onClick={handleFilterButtonClick}
+        >
+          <img className={styles.filterIcon} src={filterIcon} alt='filter'></img>
+        </button>
+      </div>
       <button 
-        className={cx({
-          searchFilterButton: true,
-          searchFilterButtonClicked: isSearchFilterOpen,
-        })}
-        onClick={handleFilterButtonClick}
+        className={styles.searchBarButton}
+        onClick={onResetButtonClick}
       >
-        Filter
+        Reset
       </button>
-      <button className={styles.searchSubmitButton} onClick={() => onSearchSubmitButtonClick(searchBarValue)}>Search</button>
+      <button className={styles.searchBarButton} onClick={() => onSearchSubmitButtonClick(searchBarValue)}>Search</button>
     </div>
     {isSearchFilterOpen && (
       <>
-        <SearchFilterComponent setSearchFilterValues={setSearchFilterValues} searchFilterValues={searchFilterValues}/>
+        <SearchFilterComponent />
       </>
     )}
     </div>
